972546d040359278839a425fc33bbc72e1ec3762
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-11-19 07:48:17 +0100

    /* Inside .cpp file, app_main function must be declared with C linkage */

--- Structural_Patterns/Flyweight/main/main.cpp
+++ Structural_Patterns/Flyweight/main/main.cpp
@@ -15,8 +15,8 @@
 #include "Font.h"
 #include "GlyphContext.h"
 
-
-void app_main(void)
+/* Inside .cpp file, app_main function must be declared with C linkage */
+extern "C" void app_main(void)
 {
     printf("Hello world!\n");
 

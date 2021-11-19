bbf1601f2b407a3832630e47cd0ae2936b3124c1
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-11-19 17:42:58 +0100

    ..

--- Behavioral_Patterns/Observer/main/main.cpp
+++ Behavioral_Patterns/Observer/main/main.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "sdkconfig.h"
 
-
+#include "ClockTimer.h"
+#include "AnalogClock.h"
+#include "DigitalClock.h"
 
 /* Inside .cpp file, app_main function must be declared with C linkage */
 extern "C" void app_main(void)
 {
     printf("Hello world!\n");
 
-
-
-
+    ClockTimer*   timer        = new ClockTimer;
+    AnalogClock*  analogClock  = new AnalogClock(timer);
+    DigitalClock* digitalClock = new DigitalClock(timer);
 }
